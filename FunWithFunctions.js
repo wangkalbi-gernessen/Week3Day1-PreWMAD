df9a2caf5f2f5d0d7e021827e897a666e6b98153
Aidan Middleton <aidanemiddleton@gmail.com>
2020-08-15 15:09:26 -0700

    added 4 functions

---
+++ FunWithFunctions.js
@@ -0,0 +1,59 @@
+/*I have written out the shells of some functions that you will have to create the logic for. a couple of tips:    
+
+  1. Take your time. Think about each problem, and try to break it into its individual parts. 
+     consider writing down these steps, and then try and convert them into code
+
+  2. Run your code along the way. If you are getting strange outputs, use them to help figure out where you 
+     are making your mistake. Make sure to test every function with multiple different inputs.
+
+  3. Uncomment the function calls I have made for you at the bottom of the file in order to test your function. 
+     make sure you are getting all of the expected results
+*/
+
+
+// Function one: should have two parameters, meaning it will take two arguments. 
+// Should add two numbers, and console.log the result.
+
+const addTwoNumbers = function() {
+
+  //Your code goes here
+
+}
+
+
+// Function Two: should have two parameters, meaning it will take two arguments. 
+// Should multiply the two numbers, and console.log the result.
+
+const multiplyTwoNumbers = function() {
+
+  //Your code goes here
+
+}
+
+
+// Function Three: should have two parameters, meaning it will take two arguments. 
+// Should divide the first argument by the second, and console.log the result.
+
+const divideXByY = function() {
+
+  //Your code goes here
+
+}
+
+
+// Function Four: should have two parameters, meaning it will take two arguments. 
+// The first argument will be the total amount in dollars and cents (ex. 10.25) and the second will be the percentage of tax charged (ex. 5).
+// calculate the toal price (the amount, + the percentage)
+
+const calculateTax = function() {
+
+  //Your code goes here
+
+}
+
+
+
+
+
+
+
